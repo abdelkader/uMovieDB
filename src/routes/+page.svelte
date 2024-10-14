222c69babccc3d97c6aad619d86bf6e4d3581ff7
abdelkader <sancha@live.ca>
2024-10-14 12:14:18 -0400

    added daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,18 @@
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="card card-side bg-base-100 shadow-xl m-4">
+    <figure>
+        <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt="Movie"
+        />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Watch</button>
+        </div>
+    </div>
+</div>
